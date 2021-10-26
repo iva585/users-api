1f05c7cf9d1e007770a1a4fedddd480496c9a154
Iva Pranjic <pranjic.iva213@gmail.com>
2021-10-26 19:13:19 +0200

    add 404 status

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,26 @@
 import express from 'express';
+import { request } from 'http';
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const users = ['Iva', 'Mateo', 'Robert', 'Sascha'];
 
+app.post('/api/users', (_request, response) => {
+  response.send(_request.body);
+});
+
 app.delete('/api/users/:name', (_request, response) => {
-  const name = _request.params.name;
-  const index = users.indexOf(name);
-  if (index !== -1) {
-    users.splice(index, 1);
-    response.send(`${name} is deleted.`);
-  } else {
-    response.status(404).send('This Person was not found in the Database');
+  const index = users.indexOf(_request.params.name);
+  if (index === -1) {
+    response.status(404).send("User doesn't exist.");
+    return;
   }
+
+  users.splice(index, 1);
+  response.send('Deleted.');
 });
 
 app.get('/api/users/:name', (_request, response) => {
@@ -21,7 +28,7 @@ app.get('/api/users/:name', (_request, response) => {
   if (isNameKnown) {
     response.send(_request.params.name);
   } else {
-    response.status(404).send("Sorry can't find that!");
+    response.status(404).send("Sorry can't find that user!");
   }
 });
 

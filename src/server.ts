c1fd8d97ff5ec5713c72ca9e7ce036d200278c01
Iva Pranjic <pranjic.iva213@gmail.com>
2021-10-26 11:36:53 +0200

    request.params.name intro

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/users/:name', (_request, response) => {
+  response.send(_request.params.name);
+});
+
+app.get('/users', (_request, response) => {
+  const users = ['Iva', 'Mateo', 'Robert', 'Sascha'];
+  response.send(users);
+});
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
